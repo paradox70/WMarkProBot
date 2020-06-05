5a7e46d078754e090ea792a87da507f77cba32b5
Mehdi <mehdi.rahimi70@gmail.com>
2020-06-06 01:15:23 +0430

    Create English.py

---
+++ English.py
@@ -0,0 +1,346 @@
+RTL = '‎'
+
+LTR = '‏‏'
+
+LANGUAGE = 'Language'
+
+USER = 'User'
+
+SELECT_LANGUAGE = '🌐 Please choose your desired language:'
+
+NEW_INVITATION_NOTIF = '👥 User {} started the bot with your dedicated link.\n' \
+                       'Notification settings /notification'
+
+START_AGAIN = 'No need to start again.\n' \
+              'In case you need help, read the bot manual.'
+
+UNKNOWN_CANCEL = 'Operation successfully canceled.'
+
+CANCEL_INVALID = 'There is nothing to cancel!'
+
+TIPS_HEADER = '💎 Using this bot, you can easily watermark your photos, videos and gifs.\n\n'
+
+START_TIPS = '✔️ <b><u>📜 Manual</u></b>:\n\n' \
+             '▪️ In [🔧 logo management] you can add up to 4 logos and customize them to your liking. \n\n' \
+             '▪️ when you send a file (image, video, gif) to the bot, ' \
+             'it will send the same file back to you with inline keyboard ' \
+             'and using those keyboard you can watermark that file.\n\n' \
+             '▪️ In case you want the bot to automatically watermark photos for you, you just need to enter ' \
+             'the [⚙️ settings] and adjust the watermark automating settings as you see fit.\n\n' \
+             '▪️ Every day you can watermark up to 3 photos and one video/gif for free, ' \
+             'but if you need to watermark more photos, videos and GIFs, you can ' \
+             'buy the premium subscription.\n\n' \
+             '▪️ Before paying for the premium subscription, you can check the premium features for free for 3 days ' \
+             'and make sure that it will meet your needs. ' \
+             'This gift includes a 30MB watermark quota.\n\n' \
+             '▪️ If you have bought the premium subscription you can watermark unlimited number of photos and ' \
+             'watermark videos and GIFs until your watermark quota is up\n\n' \
+             '▪️ Every time you watermark a video or GIF, you’re watermark quota will be reduced by the file size، ' \
+             'but watermarking photos does not follow the same rule and as long as your premium subscription holds, ' \
+             'you can watermark photos. .\n\n' \
+             '▫️ In case you have any trouble contact @WMarkProBotSupport for support.'
+
+START_DEAR = 'Dear'
+
+START_WELCOME = 'Welcome to the <b>Pro Watermark Bot</b>.'
+
+TUT_HEADER = '📜 Where to begin?'
+
+TUT1 = '1- First, click/tap on [🔧 logo management] button and add a new log and customize its settings to your liking.'
+
+TUT2 = '2- Now, you just need to send the photo, video or gif that you want to watermark to the bot ' \
+       'and wait for the bot to respond. Then use the inline keyboard to watermark your file.'
+
+TUT3 = '3- In case you want the bot to automatically watermark photos for you, you just need to enter ' \
+       'the [⚙️ settings] and adjust the watermark automating settings as you see fit.'
+
+LANGUAGE_SET = '🌐 Your language was changed to {}.'
+
+QUIT_ERROR = '✖️ This message was sent more than 48 hours ago. It cannot be deleted anymore. 🙄\n\n' \
+             'You can delete it yourself or leave it be.'
+
+QUIT = '👌✖️'
+
+LIMIT_REPLY_CHARACTER = '⚠️ The number of allowed characters is {max_length} ' \
+                        'while you have entered {user_length} characters!'
+
+SHORTEN_YOUR_TEXT = 'Please shorten your text and send it again:'
+
+CANCEL = '{}/Cancel'.format(LTR)
+
+CORRECT_COMMENT = 'Please adjust your text and resend it:'
+
+PARAGRAPH_FORMAT = '⚠️ Please send your text in the form of a single line and don’t add a new line!'
+
+SELECT_LOGO_POSITION = '👉 Please define the position of your logo:'
+
+LOGO_SETTING = '🔧 <b>Adding and managing logos</b>:\n\n' \
+               '🔻 You can add and manage up to 4 different logos.\n' \
+               '🔻 In order to add a new logo click/tap on the [➕ New logo] button.\n' \
+               '🔻 To customize each logo, click/tap on that logo’s button.'
+
+SEND_LOGO = '🖼 There are a few ways to add new logos and customize them:\n\n' \
+            '1- Send your desired text to the bot (e.g. @RobomizBot). ' \
+            'In this case, a default telegram logo will be created for you, ' \
+            'which you can then customize it using the tools at your disposal.\n\n' \
+            '2- Use image editing apps (e.g. Photoshop, PicsArt, etc) and make your own transparent logo ' \
+            'and then send it to the bot (‘Send as file’).\n\n' \
+            '3- In case your image includes a black or white background ' \
+            'and you want to remove that background, just send the logo file (‘Send as photo’) ' \
+            'and the bot will give you an option to remove the background.\n\n' \
+            '4- You can also send static telegram stickers as a logo.\n\n' \
+            '👉 <b>Please send your file/text according the above instructions:</b>'
+
+LOGO_SPEC = '<b> 🔖 Logo specifications:</b>'
+
+LOGO_TEXT = '✏️ Logo text: {}'
+
+LOGO_NAME = '✏️ Logo name: {}'
+
+LOGO_OPACITY = '💧 Logo opacity: {}'
+
+LOGO_RATIO = '🔳Ratio of logo▪️ to image⬜ size: {}'
+
+LEFT_RIGHT = '↔ Left and right margin: {}'
+
+UP_DOWN = '↕ Top and bottom margin: {}'
+
+CHANGE_LOGO_SPEC = '👉 To change each one of these specifications, choose the corresponding button.'
+
+SUBMIT_TEXT_LOGO = '👉 Please send the logo text in the form of a single line, using no more than 50 characters:'
+
+SUBMIT_NAME_LOGO = '👉 Please send a name for your logo in the form of a single line, using no more than 20 characters:'
+
+NEW_LOGO_CREATED = '✔️ The new logo is ready.'
+
+SUBMIT_LOGO_COLOR = '🎨 Please choose a color for the icon:'
+
+SUBMIT_TEXT_COLOR = '🎨 Please choose a color for the text:'
+
+SUBMIT_OPACITY = '💧 Please define the logo opacity with respect to the image in percentage:\n\n' \
+                 'Hint: the higher you set this value, the clearer your logo will be. ' \
+                 'And by lowering this value you will increase the transparency of the logo.'
+
+SUBMIT_RATIO = '🔳 Please choose ratio of the logo to the image width :\n' \
+               '🔻 You can also type in a number between 0-100!'
+
+ONLY_PHOTO = '👉 Now you need to send your logo.\n' \
+             'Please send your logo:'
+
+LOGO_ACCEPTED = '✔️ Logo received.'
+
+ONLY_RATIO = '👉 Now, you need to set the logo to image size ratio.\n' \
+             'Please type in a number between 0-100:'
+
+SET_NEW_RATIO = '✔️ The size ratio is changed to {}%.'
+
+SET_NEW_LOGO_NAME= '✔️ The logo name is set.'
+
+INSERT_MARGIN = '👉 To increase the accuracy of logo-positioning ' \
+                'you can define the space between the logo and each side of your image.\n' \
+                'To adjust each one, choose the corresponding button:'
+
+INSERT_MARGIN_W = '👉 Here you can define the space between the logo and the left or right edge of the image ' \
+                  '(depending on its position) as a percentage of image width.\n' \
+                  'If you want the logo to stick to the side, choose zero, otherwise set a larger value:'
+
+INSERT_MARGIN_H = '👉 Here you can define the space between the logo and the upper or lower edge of the image ' \
+                  '(depending on its position) as a percentage of image height.\n' \
+                  'If you want the logo to stick to the top/bottom, choose zero. otherwise set a larger value:'
+
+SET_FONT = '🎨 Please choose a font type:'
+
+REMOVE_LOGO = 'Are you sure you want to remove the log?'
+
+SELECT_LOGOTYPE = '👉 Please choose one of the provided designs:'
+
+LOGO_REMOVED = '✅ Logo was removed!'
+
+LOGO_REMOVED_TEMP = '✅ Logo is removed temporarily!\n\n' \
+               'To apply the changes, click/tap on the [✅ Finalize watermark] button.'  # TODO
+
+NO_LOGO_YET = '🔻 You have not set a logo yet. To add a new logo click/tap on [🔧 logo management] button.'
+
+JUST_PRO_WATERMARK = '⚠️ Only Premium users can attach their logo on images that they send.'
+
+SUBMIT_STROKE_COLOR = '🎨 Please pick the color of the text stroke:'
+
+AUTO_RATIO = '✅ The size ratio is set to Auto!\n' \
+             'In Auto mode, the size ratio will be set according to the logo and image size.'
+
+AUTO_STAT = 'Auto mode'
+
+WHITE_TO_TRANSPARENT = '✅ White background was successfully removed.'
+
+BLACK_TO_TRANSPARENT = '✅ Black background was successfully removed.'
+
+LOGO_TEST_WATERMARKED = '✅ The photo was successfully watermarked.'
+
+LOGO_WATERMARKED = 'The photo was watermarked.'
+
+LOGO_WATERMARKED_TEMP = '✅ THe thumbnail was watermarked.\n\n' \
+                        '👉 To apply the changes, click/tap on the [✅ Finalize watermark] button.'  # TODO
+
+ONLY_TEXT = '⚠️ Please send a text message:'
+
+SPAM_LIMIT = '⚠️ In order to counter bot attacks and prevent the bot from slowing down, ' \
+             'users can not send more than 20 messages in a minute!'
+
+PER_DAY_LIMIT = '⚠️ As a free subscriber, you can only watermark 3 images per day.\n' \
+                'To get rid of this limitation, please buy the premium subscription.\n' \
+                '{}/upgrade'.format(LTR)
+
+VIDEO_PER_DAY_LIMIT = '⚠️ As a free subscriber, you can only watermark 1 video or GIF per day.\n' \
+                'To get rid of this limitation, please buy the premium subscription.\n' \
+                '{}/upgrade'.format(LTR)
+
+FREE_LIMIT = '⚠️ As a free subscriber, you are not allowed to watermark video’s or gif files.\n' \
+                'To get rid of this limitation, please buy the premium subscription.\n' \
+                '/upgrade'.format(LTR)
+
+BE_PATIENT = 'Please be patient 🙏'
+
+WATERMARK_ALLOWED = 'Please send a photo, a video or a gif file:'
+
+LOW_WATERMARK_VOLUME = '⚠️ The size of this video exceeds your watermark quota!\n' \
+                       'To increase your watermark quota, click/tap on [💎 Premium Subscription] button.'
+
+# JUST_PRO_WATERMARK_VIDEO = '⚠️ Watermarking videos and gif is limited to premium users.\n' \
+#                            'To get premium subscription, click/tap on [💎 Premium Subscription] button.'
+
+LIMITED_WATERMARK_SIZE = '⚠️ Currently, the max file size of a video to be watermarked is 20MB!\n\n' \
+                         'Watermarking larger files will be possible in the future by upgrading our servers.'
+
+WATERMARK_PRO_VOLUME = 'Remaining watermark quota'
+
+PRO_SUBSCRIPTION = 'Premium subscription validity date'
+
+ACTIVE_TILL = 'Active till'
+
+DEACTIVE_FROM = 'Deactive from'
+
+DEACTIVE = 'Deactive'
+
+NO_LANGUAGE = 'Unknown'
+
+SETTINGS = 'Settings'
+
+SELECT_LOGOS = '👉 In case you want your images to be watermarked automatically and instantly ' \
+               'You just need to pick a logo and define its default position.'
+
+NO_LOGOS = '⚠️ You haven’t added any logos yet.\n' \
+                ' 👉 Please add a logo form [logo management] section and try again.'
+
+AUTO_LOGO_DEACTIVATED = '☑️ Automatic watermarking logo is deactivated.'
+
+AUTO_LOGO_ACTIVATED = '✅ Automatic watermarking logo is activated!'
+
+VIP_INSERTED = '#Renew subscription\n' \
+               'User: {} #u{}\n' \
+               'Days: {}\n' \
+               'Old date: {},  new date: {}\n' \
+               'Volume: {}\n' \
+               'Old volume: {} MB, new volume: {} MB'
+
+PROMOTE_BY_ADMIN_HEADER = 'Dear {}:\n\n'
+
+TIME_PROMOTION = '✅ Your premium subscription was renewed and is valid until {}. \n\n'
+
+VOL_INCREASE = '✅ Your watermark quota was increased to {} MB. \n\n'
+
+PROMOTE_BY_ADMIN_FOOTER = 'Thank you for your purchase 🙏😊'
+
+END_OF_PROMOTION = 'Dear user {}:\n\n' \
+                   '⚠️ Your premium subscription has come to an end.\n' \
+                   '👉 To renew your subscription you just need to send /upgrade and act accordingly.\n\n' \
+                   'Thank you 🙏😊'
+
+SUBMIT_ICON_SIZE = '🎨 Please define the icon size:'
+
+CURRENT_SUBSCRIPTION = 'Your current subscription: {}'
+
+FREE = 'Free'
+
+PREMIUM = 'Premium'
+
+PREMIUM_NOTICE = '🔶 Things to keep in mind before buying a premium subscription'
+
+PREMIUM_UNLIMITED_PHOTO = '🔸 A premium subscription enables you to watermark an unlimited number of images ' \
+                          'before your subscription ends.'
+
+PREMIUM_VIDEO_SIZE_LIMIT = '🔸 Currently, using the premium subscription the max video/gif size ' \
+                           'you can watermark is 20MB.' \
+                           ' (Watermarking larger files will be possible in the future by upgrading our servers.)'
+
+PREMIUM_FREE = '🔸 Using our free subscription plan you can watermark up to 3 photos and one video/GIF per day.'
+
+PREMIUM_FREE_TRIAL = '🔸 Before attempting to buy the premium subscription ' \
+                     'you can check out the bot’s performance and features using our 3-day free trial.'
+
+PREMIUM_VOLUME = '🔸 Upon the end of your premium subscription, your watermark quota will not be lost ' \
+                 'and after renewing your subscription it will be added to your purchased quota.'
+
+PREMIUM_WATERMARK_VOLUME = '🔸 You can renew your subscription or increase your watermark quota at any time. ' \
+                           'In the case, your purchased quota will be added to your current quota.'
+
+PREMIUM_PLANS = '📜 Premium plans:'
+
+PREMIUM_ONE = '➊ 📦 1 month subscription + 200MB watermark quota ---» ($ 4.99)'
+
+PREMIUM_TREE = '➋ 📦 3 month subscription + 600MB watermark quota ---» ($ 11.99)'
+
+PREMIUM_SIX = '➌ 📦 6 month subscription + 1200MB watermark quota ---» ($ 17.99)'
+
+PREMIUM_TWELVE = '➍ 📦 12 month subscription + 2400MB watermark quota ---» ($ 23.99)'
+
+EXTRA_VOLUME = '🔵 In case you need to increase your watermark quota you can purchase one of the below options:'
+
+VOLUME1 = '➎ 🎞 200MB ---» (1.99 $)'
+
+VOLUME2 = '➏ 🎞 500MB ---» (3.99 $)'
+
+VOLUME3 = '➐ 🎞 1500MB ---» (9.99 $)'
+
+VOLUME4 = '➑ 🎞 5000MB ---» (24.99 $)'
+
+PAYMENT_DESCRIPTION = '🔻 Depending on your needs, choose one of the subscription plans ' \
+                      'and complete your payment using one of the methods provided.'
+
+PRO_ACTIVATED = '🔥🎁 Free 3-day premium subscription with 30MB watermark quota is activated for you:'
+
+START = '📅 Start:'
+
+END = '📅 End:'
+
+PRO_ACTIVATE_BEFORE = '⚠️ Free premium subscription has already been activated for you!\n\n'
+
+PAYMENT_METHODS = 'Please pay for your desired plan using one of the methods provided and then ' \
+                  ' send your ID along with your purchase details to our support team.\n' \
+                  'Your ID: {}\n' \
+                  'Support: @WMarkProBotSupport'
+
+PAYMENT_HEADER = '🏦 Payment methods:'
+
+PAYPAL = '💳 Pay with Paypal'
+
+PAYPAL_DESCRIPTION = 'Pay through the link below.\n' \
+                     'https://paypal.me/robomizbot'
+
+CRYPTOCURRENCY = '💎 Pay using the cryptocurrencies'
+
+CRYPTOCURRENCY_DESCRIPTION = 'Multiply the amount in dollar by the desired currency’s exchange rate ' \
+                             'and transfer the result to the provided wallet address.'
+
+EXCHANGE_RATE = 'Exchange rate: {}'
+
+BITCOIN = 'Bitcoin'
+
+ETHEREUM = 'Ethereum'
+
+LITECOIN = 'Litecoin'
+
+TETHER = 'Tether'
+
+WALLET_ADDRESS = 'Wallet address: {}'
+
+OTHER_CRYPTO = '🔻 To pay using other currencies contact our support team @WMarkProBotSupport.'
